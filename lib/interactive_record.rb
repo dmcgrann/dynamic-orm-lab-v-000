b5b74e6f8e429746ca24263590bca3108f2f4c8f
David McGrann <dpmcgrann@gmail.com>
2018-07-14 17:40:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,8 +59,8 @@ class InteractiveRecord
   end
 
   def self.find_by(att)
-    sql = "SELECT * FROM #{self.table_name} WHERE CAST (#{att} AS TEXT) '#{att}' = '#{att}'"
-    DB[:conn].execute(sql)
+    sql = "SELECT * FROM #{self.table_name} WHERE CAST '#{att}' = '#{att}'"
+    DB[:conn].execute(sql).to_s
   end
 
 end
