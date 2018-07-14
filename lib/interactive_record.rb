1f47e3b437b00b50cd4c4c4150dbc3bc85606222
David McGrann <dpmcgrann@gmail.com>
2018-07-14 17:45:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,7 @@ class InteractiveRecord
   end
 
   def self.find_by(att)
-    sql = "SELECT * FROM #{self.table_name} WHERE CAST(colname as VARCHAR(10)) LIKE '123%' '#{att}' = '#{att}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE CAST(colname as VARCHAR(10)) '#{att}' = '#{att}'"
     DB[:conn].execute(sql)
   end
 
